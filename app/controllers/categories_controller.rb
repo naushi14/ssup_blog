c9febdf49c6332bd22788ef64047b3e8a836dce3
Nausheen Siddiqui <nausheensddq@gmail.com>
2020-10-01 13:56:40 +0530

    create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
 
     def new
+        @category = Category.new
+    end
 
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+          flash[:notice] = "Category was successfully created"
+          redirect_to @category
+        else
+          render 'new'
+        end
     end
   
     def index
@@ -12,8 +22,11 @@ class CategoriesController < ApplicationController
   
     end
 
-    def create
-    
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
     end
+   
 
 end
